82a603dfa9f676cda24700ede3dc918f962a499d
martsdag <clarte.lisse.31@gmail.com>
2025-01-20 18:33:58 +0400

    draft: notification

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ const nextPageButton = document.getElementById('next-page-button');
 let pdf = null;
 let currentPage = 1;
 
+// createContextualFragment преобразует строки в DOM елементы
+const createFragment = (string) => new Range().createContextualFragment(string);
+
+const notificationElement = `<div class="notification" id="notification">
+          <button class="button-close" onclick="onClickClose()">
+            <img
+              alt="close-thick"
+              src="/src/assets/icons/close-outline.svg"
+              class="icon"
+            >
+          </button>
+          <button class="button" onclick="onClickContinueReading()">Продолжить чтение</button>
+          <p class="alert__text">Этот файл уже был загружен. Желаете ли продолжить его чтение?</p>
+        </div>
+`;
+
 const renderPage = () => {
   // TODO: написать обработку исключений
   pdf
@@ -28,6 +44,24 @@ const onChangeUploadedFile = (event) => {
   if (!file) {
     return;
   }
+
+  const savedPdfFile = localStorage.getItem('saved-pdf-file');
+
+  if (savedPdfFile) {
+    const { name, size } = JSON.parse(savedPdfFile);
+
+    if (!name === file.name && size === file.size) {
+      return;
+    }
+
+    document.body.append(createFragment(notificationElement));
+  }
+
+  localStorage.setItem(
+    'saved-pdf-file',
+    JSON.stringify({ name: file.name,
+      size: file.size }),
+  );
 
   const fileReader = new FileReader();
 
@@ -73,4 +107,21 @@ const updateButtonsDisability = () => {
 
   previousPageButton.disabled = currentPage <= 1;
   nextPageButton.disabled = currentPage >= pdf.numPages;
+};
+
+const onClickContinueReading = () => {
+  const notification = document.getElementById('notification');
+
+  notification.remove();
+  setCurrentPage(currentPage);
+};
+
+const onClickClose = () => {
+  const notification = document.getElementById('notification');
+
+  if (!notification) {
+    return;
+  }
+
+  notification.remove();
 };
